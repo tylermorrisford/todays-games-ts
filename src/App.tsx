16bb051ce0702446845d12b255001e04a5caedda
Tyler Morris Ford <tylerford@gmail.com>
2022-04-06 00:00:41 -0600

    refactor generalized leaders component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
 import './App.css';
-import { GoalLeaders } from './Components/GoalLeaders';
+import { Leaders } from './Components/Leaders';
 import { TodayGames } from './Components/TodayGames';
 
+interface ResponseObject {
+  [key: string]: any
+}
+
 export default function App() {
+
+  const [season, setSeason] = React.useState('')
+
+  const leaderCategories: Array<string> = ['goals', 'assists', 'points', 'shots', 'timeOnIcePerGame', 'faceOffPct', 'plusMinus', 'shootingPctg', 'hits', 'penaltyMinutes']
+  const goaltenderCategories: Array<string> = ['wins', 'gaa', 'shutouts', 'savePct', 'losses', 'otLosses']
+
+  React.useEffect(() => { // Grab season id on load
+    fetch('https://statsapi.web.nhl.com/api/v1/seasons/current')
+        .then(res => res.json())
+        .then((data: ResponseObject = {}) => {
+            console.log('current season:', data)
+            setSeason(data.seasons[0].seasonId)
+        })
+}, [])
+
   return (
     <div className="App">
       <TodayGames />
       <hr />
-      <GoalLeaders />
+      <p style={{fontSize: '1.2em'}}><em>Player Leaders</em></p>
+      {leaderCategories.map((cat: string, i: number) => {
+        return <>
+          <Leaders season={season} category={cat} key={cat} />
+          {i === leaderCategories.length - 1 ? '' : <hr style={{width: '50%'}} />}
+        </>
+      })}
+      <hr />
+      <p style={{fontSize: '1.2em'}}><em>Goaltender Leaders</em></p>
+      {goaltenderCategories.map((cat: string) => {
+        return <>
+          <Leaders season={season} category={cat} key={cat} />
+          <hr style={{width: '50%'}} />
+        </>
+      })}
     </div>
   );
 }
---
+++ src/Components/Leaders.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+// generalize this component so it takes a leader category as a prop and renders a top 5 list
+// EXAMPLE of the leaders API - will return an empty response without category/season
+// https://statsapi.web.nhl.com/api/v1/stats/leaders?leaderCategories=goals&season=20202021.
+
+// GET https://statsapi.web.nhl.com/api/v1/seasons/current --> returns the current season
+// also may be helpful to pass as a prop to this component
+interface LeaderProps {
+    season: string,
+    category: string
+}
+
+interface ResponseObject {
+    [key: string]: any
+}
+
+export const Leaders: React.FunctionComponent<LeaderProps> = ({season, category}): JSX.Element => {
+    const [leaders, setLeaders] = React.useState([])
+
+    // React.useEffect(() => { // Grab season id on load
+    //     fetch('https://statsapi.web.nhl.com/api/v1/seasons/current')
+    //         .then(res => res.json())
+    //         .then((data: ResponseObject = {}) => {
+    //             console.log('current season:', data)
+    //             setSeason(data.seasons[0].seasonId)
+    //         })
+    // }, [])
+
+    React.useEffect(() => {
+        if (season !== '') { // if season exists, use it to fetch leaders
+            fetch(`https://statsapi.web.nhl.com/api/v1/stats/leaders?leaderCategories=${category}&season=${season}`)
+                .then(res => res.json())
+                .then((data: ResponseObject = {}) => {
+                    console.log(`${category} leaders`, data)
+                    setLeaders(data.leagueLeaders[0].leaders)
+                })}
+    }, [season, category])
+
+    return(
+        <>
+        {leaders.length > 0 ? <div> {category} Leaders
+            {leaders.map( (leader: any, i: number) => {
+                return <p key={i}>{leader.person.fullName}, {leader.team.name} - {leader.value}</p>
+            })}</div>
+        : <div>Loading leaders...</div>
+        }   
+        </>
+    )
+}
